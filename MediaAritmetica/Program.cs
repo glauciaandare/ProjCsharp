0f804d8aa67c865457e46dc527caf2feddbf626a
Glaucia Andare <glaucia.andare@gmail.com>
2022-10-29 18:03:00 -0300

    Criando novo projeto Aluguel de quartos

--- MediaAritmetica/Program.cs
+++ MediaAritmetica/Program.cs
@@ -1,8 +1,23 @@
-﻿namespace MediaAritmetica {
+﻿using System.Globalization;
+
+namespace MediaAritmetica {
     internal class Program {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int n = int.Parse(Console.ReadLine());
+            double[] vect = new double[n];
+            for (int i = 0; i < n; i++)
+            {
+                vect[i] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+            double sum = 0.0;
+            for (int i = 0; i < n; i++)
+            {
+                sum += vect[i];
+            }
+            double avg = sum / n;
+            Console.WriteLine("AVERAGE HEIGHT = " + avg.ToString("F2", CultureInfo.InvariantCulture));
+
         }
     }
 }
